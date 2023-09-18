b1a444ae27b5f27a7a2a0dead4149b5132c2021b
Aisa Luana <aisalgmb@gmail.com>
2023-09-18 17:38:06 -0300

    Build a System that makes orders.

---
+++ clients_order/src/clients_order/Main.java
@@ -0,0 +1,11 @@
+package clients_order;
+
+public class Main {
+
+	public static void main(String[] args) {
+        // Ler os dados de um pedido com N itens (N fornecido pelo usuário). Depois mostrar um sumário do pedido. Nota: o instante do pedido deve ser o instante do sistema: new Date().
+		
+		
+	}
+
+}
