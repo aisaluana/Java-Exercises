1150bbfdab95ef78251813656847d9e2486a168e
Aisa Luana <aisalgmb@gmail.com>
2023-10-06 12:52:26 -0300

    Build an Entities package + Entities superclass Product for name and price of products.

---
+++ imported_used_products/src/imported_used_products/Main.java
@@ -0,0 +1,11 @@
+package imported_used_products;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Build a program to read N products data (N typed by user). At the end, show the PRICE TAGS of each product the same order they've been typed.
+		// Every product has NAME and PRICE. Imported products have a CUSTOMS FEE, and used products have a MANUFACTURE DATE. These specific data should be added to the price tag. For imported products, the customs fee should be add to the final price of the product.
+
+	}
+
+}
