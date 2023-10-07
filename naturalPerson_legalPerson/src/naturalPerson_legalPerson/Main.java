4d0fcbc73e4ad038caf9042571230dabf312bc64
Aisa Luana <aisalgmb@gmail.com>
2023-10-07 19:25:39 -0300

    Build a program for monitoring natural and legal people's taxes and its statement.

---
+++ naturalPerson_legalPerson/src/naturalPerson_legalPerson/Main.java
@@ -0,0 +1,16 @@
+package naturalPerson_legalPerson;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//Fazer um programa para ler os dados de N contribuintes (N fornecido pelo usuário), os quais podem ser PESSOA FÍSICA ou PESSOA JURÍDICA, e depois mostrar o valor do IMPOSTO PAGO por cada um, bem como o TOTAL DE IMPOSTO arrecadado.
+
+	}
+
+}
