e0301aa56f3675858033d508c9171f8ddda7b51c
Aisa Luana <aisalgmb@gmail.com>
2023-10-07 19:37:40 -0300

    Build an Abstract Entities class for the common variables and methods for TaxPayers.

---
+++ naturalPerson_legalPerson/src/entities/Individual.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class Individual {
+	
+	//Regras de cálculo do imposto para Pessoa Física: pessoas cuja renda foi abaixo de 20000,00 pagam 15% de imposto. Pessoas com renda de 20000,00 em diante pagam 25% de imposto. Se a pessoa teve gastos com saúde, 50% destes gastos são abatidos no imposto.
+		//Por exemplo: uma pessoa cuja renda foi 50000,00 e teve 2000,00 em gastos com saúde,o imposto fica: (50000*25%)-(2000*50%)=11500,00
+}
