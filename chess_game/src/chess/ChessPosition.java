a1f937851c359762e014c90172dba429d95d3d99
Aisa Luana <aisalgmb@gmail.com>
2024-03-05 17:34:26 -0300

    Little Improvement in BoardPrint

--- chess_game/src/chess/ChessPosition.java
+++ chess_game/src/chess/ChessPosition.java
@@ -24,16 +24,16 @@ public class ChessPosition {
 	}
 
 	protected Position toPosition() {
-		return new Position(8-row, column - 'a');
+		return new Position(8 - row, column - 'a');
 	}
-	
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a'-position.getColumn()), 8-position.getRow());
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
 	}
-	
+
 	@Override
 	public String toString() {
-		return "" + column + row; //força o programa a concatenar as strings
+		return "" + column + row;
 	}
 
 }
